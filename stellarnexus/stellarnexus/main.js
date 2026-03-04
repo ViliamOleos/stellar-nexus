a0ded77fae1514ebb199516a7223f45d11c42a11
William <viliamoleosdev@gmail.com>
2026-03-05 01:14:20 +0500

    +install/nginx.conf; .gitignore*>[!/install/**]; main.js*>(websocket)

--- stellarnexus/stellarnexus/main.js
+++ stellarnexus/stellarnexus/main.js
@@ -1,1 +1,5 @@
-alert("gaeming!")
+var dcws = new WebSocket("wss://gateway.discord.gg/?v=10&encoding=json");
+
+dcws.addEventListener("message", (msg) => {
+	console.log("Message from Discord: ", msg.data)
+});
