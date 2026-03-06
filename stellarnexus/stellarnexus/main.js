a374af3ecef443cf92a563bf5b487126e36361eb
William <111227194+ViliamOleos@users.noreply.github.com>
2026-03-07 04:22:06 +0500

    main.js*>(networking testing WIP); nginx.conf*>(fixed notfound bug)
    The Discord networking kinda works, but not quite yet. Also fixed a bug in the server config itself.

--- stellarnexus/stellarnexus/main.js
+++ stellarnexus/stellarnexus/main.js
@@ -1,5 +1,29 @@
 var dcws = new WebSocket("wss://gateway.discord.gg/?v=10&encoding=json");
 
-dcws.addEventListener("message", (msg) => {
-	console.log("Message from Discord: ", msg.data)
+function snore(ms) { return new Promise(res => setTimeout(res,ms)); }
+
+/// MAIN ///
+
+var seqnum = null;
+function heartbeat(seqnum) {
+	var json = new Object();
+	json.op = 1;
+	json.d = seqnum;
+	console.log("sent heartbeat");
+	dcws.send(JSON.stringify(json));
+}
+
+/// MSGS ///
+
+dcws.addEventListener("message", async (content) => {
+	var data = JSON.parse(content.data);
+
+	seqnum = data.s;
+	console.log(data);
+
+	switch(data.op) {
+		case 10: // HELLO
+			await snore(heartbeat * Math.random());
+			heartbeat(seqnum);
+	}
 });
