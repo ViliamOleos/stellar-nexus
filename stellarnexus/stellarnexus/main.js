80f7fed26c42fefc6e81fd29ab07ec71bf27243c
Greyzcoolstuff <nidzojlo1@gmail.com>
2026-03-09 22:15:43 +0100

    Heartbeat thingy main.js

--- stellarnexus/stellarnexus/main.js
+++ stellarnexus/stellarnexus/main.js
@@ -6,28 +6,51 @@ function snore(ms){
 }
 
 /// MAIN ///
-
 var seqnum = null;
-function heartbeat(seqnum) {
-	var json = new Object();
-	json.op = 1;
-	json.d = seqnum;
-	console.log("sent heartbeat");
-	dcws.send(JSON.stringify(json));
+var heartbeatACK;
+var heartbeat_interval = 41250;
+
+function heartbeat(seqnum){
+    if(dcws.readyState === WebSocket.OPEN){  // only send if open
+        var json = new Object();
+        json.op = 1;
+        json.d = seqnum;
+        console.log("sent heartbeat");
+        dcws.send(JSON.stringify(json));
+    } else {
+        console.log("can not send heartbeat due to being offline")
+   }
 }
 
 /// MSGS ///
-
 dcws.addEventListener("message", (content) => {
 	snore(1000);
 	var data = JSON.parse(content.data);
 
 	seqnum = data.s;
 	console.log(data);
-
+/// SHUTTING DOWN ///
+dcws.onclose = e => console.log("Offline:", e);
+// I was gonna add something more here but i forgot what lowk
+	
+/// CASES ///
 	switch(data.op) {
 		case 10: // HELLO
-			snore(heartbeat * Math.random());
+			snore(Math.random() * heartbeat_interval);
 			heartbeat(seqnum);
+			setInterval(() => heartbeat(seqnum), heartbeat_interval);
+		break;
+			
+        case 11: // HEARTBEATACK
+			console.log("heartbeat ACK");
+			heartbeatACK = true;
+		break;
+			
+		case 7: // RECONNECT
+			console.log("server wants reconnect :d");
+			dcws.close();
+		break;
+
+		
 	}
 });
