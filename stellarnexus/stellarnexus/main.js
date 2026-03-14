523db6bdea3a7ba4c2567f102be69a71cb3e4651
William <111227194+ViliamOleos@users.noreply.github.com>
2026-03-14 06:24:25 +0500

    main.js*>(refactoring),(ackfix),(optimisation),(working heartbeatInterval)
    Refactored pretty much all of [main.js], basically making it eye candy. But we don't forget about optimisation here:
    
    Optimised a bit out of the logic, removing an unnecessary snore and changing up some code.
    
    [heartbeatACK] now explicitly starts at false, though our program still doesn't panik when it doesn't receive an ACK.
    
    Most functionally importantly, [heartbeatInterval] is properly set and used in this commit.

--- stellarnexus/stellarnexus/main.js
+++ stellarnexus/stellarnexus/main.js
@@ -1,62 +1,57 @@
+////////////////////////////////////////// DCWS //////////////////////////////////////////
+
 var dcws = new WebSocket("wss://gateway.discord.gg/?v=10&encoding=json");
+dcws.onclose = e => console.log(e);
 var hbid = 0;
 
-function dcws_shutdown() { 
-	clearInterval(hbid);
-	dcws.close();
-}
+function dcws_shutdown() { clearInterval(hbid); dcws.close(); }
 
-function snore(ms){
-	const end = Date.now() + ms;
-	while (Date.now() < end) { /* wait or something */ }
-}
+////////////////////////////////////////// LIBS //////////////////////////////////////////
 
-/// MAIN ///
+function snore(ms){ const end = Date.now() + ms; while(Date.now() < end) { /* ... */ } }
+
+////////////////////////////////////////// MAIN //////////////////////////////////////////
+
 var seqnum = null;
-var heartbeatACK;
-var heartbeat_interval = 41250;
+var heartbeatACK = false;
+var heartbeatInterval;
 
-function heartbeat(seqnum){
-    if(dcws.readyState === WebSocket.OPEN){  // only send if open
-        var json = new Object();
-        json.op = 1;
-        json.d = seqnum;
-        console.log("sent heartbeat");
-        dcws.send(JSON.stringify(json));
-    } else {
-        console.log("can not send heartbeat due to being offline")
-   }
+function heartbeat(){
+	var json = new Object();
+		json.op = 1;
+		json.d = seqnum;
+	dcws.send(JSON.stringify(json));
+	console.log("Sent Heartbeat: ", json);
 }
 
-/// MSGS ///
 dcws.addEventListener("message", (content) => {
-	snore(1000);
 	var data = JSON.parse(content.data);
-
 	seqnum = data.s;
+
 	console.log(data);
-/// SHUTTING DOWN ///
-dcws.onclose = e => console.log("Offline:", e);
-// I was gonna add something more here but i forgot what lowk
 	
-/// CASES ///
 	switch(data.op) {
-		case 10: // HELLO
-			snore(Math.random() * heartbeat_interval);
-			heartbeat(seqnum);
-			hbid = setInterval(() => heartbeat(seqnum), heartbeat_interval);
-		break;
-			
-        case 11: // HEARTBEATACK
-			console.log("heartbeat ACK");
+        case 11: // ACK
+			console.log("Discord acknowledged Heartbeat");
 			heartbeatACK = true;
+		break;
+
+		case 10: // HELLO
+			heartbeatInterval = data.d.heartbeat_interval;
+			snore(Math.random() * heartbeatInterval); heartbeat();
+			// setInterval will heartbeat every [heartbeatInterval]ms
+			hbid = setInterval(() => heartbeat(), heartbeatInterval);
+		break;
+
+		case 1: // HB
+			heatbeat();
 		break;
 			
 		case 7: // RECONNECT
-			console.log("server wants reconnect :d");
+			console.log("Discord wants reconnect");
 			dcws_shutdown();
 		break;
-
-		
 	}
 });
+
+//////////////////////////////////////////////////////////////////////////////////////////
